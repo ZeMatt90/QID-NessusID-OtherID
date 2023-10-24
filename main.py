d7bda86e434dd309e0d30fb3826c9e5112a97a69
Matteo Zeni <matteo.zeni@gpi.it>
2023-10-24 21:50:46 +0200

    upgrade data from nessus

--- main.py
+++ main.py
@@ -6,37 +6,47 @@ import subprocess
 print("imported all motors successfully from Arcadia\n" )
 #print(canederli.HEADER1)
 
+data_handler = data_handling.DataHandler()
+data_handler.load_data()
+#data_handler.display_summary()
+riga = data_handler.qualysdata[data_handler.qualysdata['QID'] == 38170]
+print(riga)
+riga = data_handler.qualysdata[data_handler.qualysdata['QID'] == 38173]
+print(riga)
+riga = data_handler.qualysdata[data_handler.qualysdata['QID'] == 38169]
 
 if __name__ == '__main':
     #processo padre
     # Istanziamento della classe
+    print ("1")
     data_handler = data_handling.DataHandler()
     data_handler.load_data()
     #data_handler.display_summary()
-    
-    
+    riga = data_handler.qualysdata[data_handler.qualysdata['qid'] == 38170]
+    print(riga)
+    print ("2")
     # Definisci il comando per avviare l'app Streamlit
-    command = ["streamlit", "run", "user_interface.py"]  # Sostituisci con il nome corretto del tuo file Streamlit
+    #command = ["streamlit", "run", "user_interface.py"]  # Sostituisci con il nome corretto del tuo file Streamlit
 
         # Avvia l'app Streamlit come processo figlio
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     
     # Attendi che il processo figlio termini
-    process.wait()
+    #process.wait()
 
     # Leggi l'output standard e gli errori
-    output, errors = process.communicate()
+    #output, errors = process.communicate()
 
     # Verifica eventuali errori
-    if process.returncode != 0:
-        print(f"Errore nell'esecuzione. Codice di uscita: {process.returncode}")
-        print("Errori standard:")
-        print(errors.decode())
-    else:
-        print("Esecuzione completata con successo")
-        print("Output standard:")
-        print(output.decode())
+    #if process.returncode != 0:
+    #    print(f"Errore nell'esecuzione. Codice di uscita: {process.returncode}")
+    #    print("Errori standard:")
+    #    print(errors.decode())
+    #else:
+    #    print("Esecuzione completata con successo")
+    #    print("Output standard:")
+    #    print(output.decode())
     # print("istanziati i dati e caricati in memoria al modico prezzo di %s byte",sys.getsizeof(data_handler))
     # total_memory_usage = 0
     # for df in [data_handler.nessusdata, data_handler.qualysdata, data_handler.arcadiadata]:
